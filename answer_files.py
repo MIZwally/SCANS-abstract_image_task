4f897602336dc9dca9c33583591b1cc9eeaede77
MIZwally <mizwally5@gmail.com>
2026-06-29 17:16:27 -0400

    added new rt calculation for dyads 10 and higher

--- answer_files.py
+++ answer_files.py
@@ -2,12 +2,22 @@ import pandas as pd
 import datetime as dt
 import ast
 
-def calc_rt(start, rt, diff, i) :    
-    rt = dt.datetime.strptime(rt, '%H:%M:%S')
-    rt_act = rt - (start + dt.timedelta(0,(diff.total_seconds()/6)*i))
+
+pl1_file = pd.read_csv('Data/real-data/DYAD_09_SCAN_091_abstract-images.csv')
+pl2_file = pd.read_csv('Data/real-data/DYAD_09_SCAN_092_abstract-images.csv')
+dyad_id = 9
+
+def calc_rt(start, irt, diff, i, img) :    
+    rt = dt.datetime.strptime(irt, '%H:%M:%S')
+    if dyad_id < 10 :
+        rt_act = rt - (start + dt.timedelta(0,(diff.total_seconds()/6)*i))
+    else :
+        img_t = dt.datetime.strptime(img, '%H:%M:%S')
+        rt_act = rt - img_t
+        print(img_t, rt, rt_act)
     return rt_act.total_seconds()
 
-def check_correct(director, guessor, inputs, rts, start, diff) :
+def check_correct(director, guessor, inputs, rts, start, diff, image_times) :
     #see when guessor image occurs in director list, then compare to answer in box
     graded_set = []
     #get accuracy % and round rt
@@ -20,26 +30,28 @@ def check_correct(director, guessor, inputs, rts, start, diff) :
         all_incorrect_responses = []
         all_incorrect_rt = []
         num_responses = 0
+        image_time = image_times[i]
         for j, g, in enumerate(guessor) :
             incorrect_indices = []
             incorrect_responses = []
             incorrect_rt = []
             inp = ast.literal_eval(inputs[j])
             if str(i+1) in inp :
-                if d == g :      
+                if d == g :    
                     correct_indices = [k for k, x in enumerate(inp) if x == str(i+1)]    
                     incorrect_responses = [x for x in inp if x != str(i+1)]
                     num_responses = len(inp)       
                     crt_set = [ast.literal_eval(rts[j])[k] for k in correct_indices]
                     for rt in crt_set :
-                        correct_rt.append(calc_rt(start, rt, diff, i))                                     
+                        correct_rt.append(calc_rt(start, rt, diff, i, image_time))                                     
                     if correct_indices[-1] == len(inp)-1 :
                         accurate = True
                 else :
                     incorrect_indices = [k for k, x in enumerate(inp) if x == str(i+1)]    
                     irt_set = [ast.literal_eval(rts[j])[k] for k in incorrect_indices]
-                    for ind, rt in zip(incorrect_indices, irt_set) :
-                        incorrect_rt.append(calc_rt(start, rt, diff, ind))
+                    image_time_set = [image_times[k] for k in incorrect_indices]
+                    for ind, rt, img_t in zip(incorrect_indices, irt_set, image_time_set) :
+                        incorrect_rt.append(calc_rt(start, rt, diff, ind, img_t))
             else :
                 if d == g :
                     num_responses = len(inp)
@@ -57,13 +69,9 @@ def check_correct(director, guessor, inputs, rts, start, diff) :
         
         graded = {'image': d, 'correct?': accurate, 'correct rt': correct_rt, 'incorrect rt': all_incorrect_rt,
                   'incorrect resp box': all_incorrect_responses, 'number resp box': num_responses}
-        graded_set.append(graded)     
-    return round_accuracy/6, round_rt, graded_set 
+        graded_set.append(graded)      
+    return round_accuracy/6, round_rt, graded_set     
 
-pl1_file = pd.read_csv('/Users/mizwally/Desktop/DYAD_01-tangrams/DYAD_01_SCAN_011_abstract-images.csv')
-pl2_file = pd.read_csv('/Users/mizwally/Desktop/DYAD_01-tangrams/DYAD_01_SCAN_012_abstract-images.csv')
-#pl1_file = pd.read_csv('C:\\Users\\mizwa\\Downloads\\DYAD_01_SCAN_011_abstract-images.csv')
-#pl2_file = pd.read_csv('C:\\Users\\mizwa\\Downloads\\DYAD_01_SCAN_012_abstract-images.csv')
 pl1_director = pl1_file[pl1_file['Role'] == 'director']
 pl1_guessor = pl1_file[pl1_file['Role'] == 'guessor']
 pl2_director = pl2_file[pl2_file['Role'] == 'director']
@@ -78,7 +86,8 @@ for i in range(len(pl2_director.index)):
              'rts': list(pl1_guessor.iloc[i][16:28:2]),
              'start_time': pl2_director.iloc[i]['Round_start_time'],
              'end_time': pl2_director.iloc[i]['Round_end_time'],
-             'control': pl1_guessor.iloc[i]['Control?']}
+             'control': pl1_guessor.iloc[i]['Control?'],
+             'image_time': list(pl2_director.iloc[i][16:28:2])}
     pl1_rounds.append(round)
 for i in range(len(pl1_director.index)):
     round = {'director': list(pl1_director.iloc[i][9:15]), 
@@ -87,7 +96,8 @@ for i in range(len(pl1_director.index)):
              'rts': list(pl2_guessor.iloc[i][16:28:2]),
              'start_time': pl1_director.iloc[i]['Round_start_time'],
              'end_time': pl1_director.iloc[i]['Round_end_time'],
-             'control': pl2_guessor.iloc[i]['Control?']}
+             'control': pl2_guessor.iloc[i]['Control?'],
+             'image_time': list(pl1_director.iloc[i][16:28:2])}
     pl2_rounds.append(round)
 
 pl1_answers = []
@@ -102,7 +112,7 @@ for i in range(len(pl1_rounds)) :
     pl1_diff = pl1_end - pl1_st
     if pl1_rounds[i]['control'] == False :
         pl1_acc, pl1_rt, pl1_ans = check_correct(pl1_rounds[i]['director'], pl1_rounds[i]['guessor'], pl1_rounds[i]['inputs'], 
-                                pl1_rounds[i]['rts'], pl1_st, pl1_diff)
+                                pl1_rounds[i]['rts'], pl1_st, pl1_diff, pl1_rounds[i]['image_time'])
         pl1_answers.append(pl1_ans)
         pl1_accs.append(pl1_acc)
         pl1_rts.append(pl1_rt)
@@ -116,7 +126,7 @@ for i in range(len(pl1_rounds)) :
     pl2_diff = pl2_end - pl2_st 
     if pl2_rounds[i]['control'] == False :
         pl2_acc, pl2_rt, pl2_ans = check_correct(pl2_rounds[i]['director'], pl2_rounds[i]['guessor'], pl2_rounds[i]['inputs'], 
-                                pl2_rounds[i]['rts'], pl2_st, pl2_diff)
+                                pl2_rounds[i]['rts'], pl2_st, pl2_diff, pl2_rounds[i]['image_time'])
         pl2_answers.append(pl2_ans)
         pl2_accs.append(pl2_acc)
         pl2_rts.append(pl2_rt)
@@ -127,7 +137,7 @@ for i in range(len(pl1_rounds)) :
 
 pl1_answer_file = pd.concat([pl1_guessor[pl1_guessor.columns[:5]].reset_index(drop=True), pd.Series(pl1_accs, name='Accuracy'), 
                              pd.Series(pl1_rts, name='RT'), pd.DataFrame(pl1_answers)], axis=1).drop(columns=['Role', 'Control?'])
-pl1_answer_file.to_csv('/Users/mizwally/Desktop/DYAD_01-tangrams/DYAD_01_SCAN_011_answers.csv', index=False)
+pl1_answer_file.to_csv('Data/answer-files/DYAD_09_SCAN_091_answers.csv', index=False)
 pl2_answer_file = pd.concat([pl2_guessor[pl2_guessor.columns[:5]].reset_index(drop=True), pd.Series(pl2_accs, name='Accuracy'), 
                              pd.Series(pl2_rts, name='RT'), pd.DataFrame(pl2_answers)], axis=1).drop(columns=['Role', 'Control?'])
-pl2_answer_file.to_csv('/Users/mizwally/Desktop/DYAD_01-tangrams/DYAD_01_SCAN_012_answers.csv', index=False)
+pl2_answer_file.to_csv('Data/answer-files/DYAD_09_SCAN_092_answers.csv', index=False)
